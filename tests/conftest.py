e0db13ebbf00180e7406dace9a53e998cd1c7877
Invicticus <129230980+Invicticus@users.noreply.github.com>
2023-04-09 19:20:05 -0500

    First Commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,45 @@
+import json
 import pytest
 import selenium.webdriver
 
 
 @pytest.fixture
-def driver():
-    driver = selenium.webdriver.Chrome()
-    driver.implicitly_wait(4)
+def config(scope='session'):
+    # Read the file
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+
+    # Assert values are acceptable
+    assert config['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
+    assert isinstance(config['implicit_wait'], int)
+    assert config['implicit_wait'] > 0
+
+    # Return config so it can be used
+    return config
+
+
+@pytest.fixture  # This decorator tells python the below function is a fixture.
+def driver(config):
+    # Initialize the ChromeDriver instance
+    if config['browser'] == 'Firefox':
+        driver = selenium.webdriver.Firefox()
+    elif config['browser'] == 'Chrome':
+        driver = selenium.webdriver.Chrome()
+    elif config['browser'] == 'Headless Chrome':
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument('headless')
+        driver = selenium.webdriver.Chrome(options=opts)
+    else:
+        raise Exception(f'Browser "{config["browser"]}") is not supported')
+
+    # Make its calls wait up to 10 seconds for elements to appear
+    driver.implicitly_wait(config['implicit_wait'])
+
+    # Return the WebDriver instance for the setup
     yield driver
+
+    # Quit the WebDriver instance for the cleanup
     driver.quit()
+
+
+
